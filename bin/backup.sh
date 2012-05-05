bb75d394018a623d5258dbc6eeeb579f1a338044
Gustavo Niemeyer <gustavo@niemeyer.net>
2012-05-05 15:44:04 -0700

    Remove backup tarball before listing.

--- bin/backup.sh
+++ bin/backup.sh
@@ -13,9 +13,8 @@ if [ -f $HOME/.nobackup ]; then
 	EXCLUDE="--exclude-from=$HOME/.nobackup"
 fi
 
-LIST=`find -maxdepth 1 | sed -n 's:./::p'`
-
 rm -f backup.tar.bz2
+LIST=`find -maxdepth 1 | sed -n 's:./::p'`
 tar cjf backup.tar.bz2 --exclude=.s3up --exclude=s3up --exclude=.nobackup $EXCLUDE $LIST
 $HOME/bin/s3up backup.tar.bz2 "labix-backup/${DATE}/${USER}@${HOST}_${TIME}_${UUID}.tar.bz2"
 rm backup.tar.bz2
